f1b20e4daa1445d02dbb30978dfec4df813ec636
EMMA-BPR <145418806+EMMA-BPR@users.noreply.github.com>
2024-03-08 17:24:19 -0600

    Add files via upload

---
+++ como hacer una calculadora.py
@@ -0,0 +1,88 @@
+a = 15
+b = 16
+def suma (): return print (a+b)
+suma ()
+
+
+a = 15
+b = 16
+def resta (): return print (a-b)
+resta ()
+
+
+
+a = 15
+b = 16
+def multiplicación (): return print (a*b)
+multiplicación ()
+
+
+
+a = 15
+b = 16
+def división (): return print (a*b)
+división ()
+
+
+
+print ("holaaa criaturita del infierno")
+print ("cual es tu nombre")
+input("ingresa tu nombre ")
+
+print ("hola denuevo "nombre ingresado"")
+
+
+
+def suma (numero1, numero2):
+ return(numero1 + numero2)
+
+def resta (numero1,  numero2):
+ return (numero1 - numero2)
+
+def multiplicación (numero1, numero2):
+ return (numero1 * numero2)
+
+def división (numero1, numero2):
+ return (numero1 / numero2)
+
+
+
+print ("cuales son tus numeros favoritos")
+input ("ingresa tus numeros favoritos")
+print ("que geniaaaal esos tambien son mis numeros favoritris")
+
+def coemnzar (): return input(empezar)
+empezar ()
+
+print ("porfavor criaturita del mal elige una operación")
+print (1.-suma)
+print (2.-resta)
+print (3.-multiplicación)
+print (4.-división)
+
+input ("ingresa una opción 1/2/3/4: ")
+
+numero1 - float(input("ingresa tu primer numero porfavor"))
+numero2 - float(input("ingresa tu segunda cantidad"))
+            
+if opcion == '2':
+    print(numero1, "+", numero2, "=", suma(numero1, numero2))
+             
+elif opción == '2':
+    print(numero1, "-", numero2, "=", resta(num1, numero2))
+
+elif opcion == '3':
+    print(numero1, "*", numero2, "=", multiplicacion(num1, num2))
+
+elif opcion == '4':
+    print(numero1, "/", numero2, "=", division(num1, num2))
+
+else:
+    print("Opción inválida")
+
+
+
+    
+
+
+
